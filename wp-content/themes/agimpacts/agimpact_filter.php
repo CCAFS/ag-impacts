5f597f06f397829990aa5e3061c73203d8196eda
camilo rodriguez <camilor67@gmail.com>
2014-11-25 08:59:19 -0500

    filter change

--- wp-content/themes/agimpacts/agimpact_filter.php
+++ wp-content/themes/agimpacts/agimpact_filter.php
@@ -20,23 +20,17 @@ get_header();
       <legend>Filter</legend>
       <div class="pure-g">
         <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
-          <label for="doi">DOI</label>
-          <input type="hidden" name="doi" id="doi"  data-placeholder="Choose An Option.." />
-        </div>
-
-        <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
-          <label for="scale">Spatial Scale</label>
-          <input type="hidden" name="scale" id="scale"  data-placeholder="Choose An Option.." />
-        </div>
-
-        <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
           <label for="crop">Crop</label>
           <input type="hidden" name="crop" id="crop" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
         </div>
-
         <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
-          <label for="model">Multi-Model Ensemble</label>
+          <label for="model">Crop Model</label>
           <input type="hidden" name="model" id="model" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
+        </div>
+        
+        <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
+          <label for="doi">Emission Scenario</label>
+          <input type="hidden" name="climate" id="climate" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
         </div>
 
         <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
@@ -56,24 +50,24 @@ get_header();
           <label for="period_end">Projection Period End</label>
           <input type="hidden" name="period_end" id="period_end" class="input-xlarge" style="width:80px;" data-placeholder="Choose An Option.." />
         </div>
+        <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
+          <label for="scale">Spatial Scale</label>
+          <input type="hidden" name="scale" id="scale"  data-placeholder="Choose An Option.." />
+        </div>
 
         <div id="location">
-          
+          <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
+            <label for="subcontinents">Continent</label>
+            <input type="hidden" name="subcontinents" id="subcontinents" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
+          </div>
+          <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
+            <label for="region">Region</label>
+            <input type="hidden" name="region" id="region" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
+          </div>
           <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
             <label for="country">Country</label>
             <input type="hidden" name="country" id="country" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
           </div>
-
-          <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
-            <label for="subcontinents">Sub-Continents</label>
-            <input type="hidden" name="subcontinents" id="subcontinents" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
-          </div>
-
-        </div>
-
-        <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
-          <label for="doi">Climate Scenario</label>
-          <input type="hidden" name="climate" id="climate" class="input-xlarge" style="width:350px;" data-placeholder="Choose An Option.." />
         </div>
 
         <div class="pure-u-1 pure-u-md-1-3" style="height: 45px">
@@ -83,10 +77,10 @@ get_header();
         <button class="pure-button pure-button-primary" type="button" name="search" id="search">Search</button>
       </div>
     </fieldset>
-    </form>
-    <p>
-    <div id="results"><h3>Results</h3></div>
-    </p>
-    <div id="success"></div>
+  </form>
+  <p>
+  <div id="results"><h3>Results</h3></div>
+</p>
+<div id="success"></div>
 </section>
 <?php get_footer(); ?>
